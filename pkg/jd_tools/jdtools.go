80c5dfc47255f64a6ad61892c08020d74ea30af2
Your Name <you@example.com>
2021-01-02 16:19:52 +0000

    up

--- pkg/jd_tools/jdtools.go
+++ pkg/jd_tools/jdtools.go
@@ -3,14 +3,21 @@ package jd_tools
 import (
 	"SnapUp/pkg/url_tools"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/chromedp/chromedp"
+	cookiejar "github.com/orirawlings/persistent-cookiejar"
+	"github.com/toqueteos/webbrowser"
 	"image"
 	"io"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
-	"net/http/cookiejar"
+	"net/http/httptest"
+	//"text/template/parse"
+
+	//cookiejar "github.com/juju/persistent-cookiejar"
 	"net/url"
 	"regexp"
 	"strconv"
@@ -19,34 +26,103 @@ import (
 
 type JdTools interface {
 	GetLoginQrcode() (qrcode image.Image, err error) //获取登录二维码
-	CheckLogin() (err error)                         //检查是否扫描登录成功
-	CheckCookies() (status bool)                     //检查cookies是否过期
+	CheckLogin() (code int, err error)               //检查是否扫描登录成功
+	GetUserInfo() (err error)                        //获取用户信息
+	CheckCookies() (code int, err error)             //检查cookies是否过期
+	AutoObtainEidFp() (err error)                    //自动获取eid和fp
+	ManualObtainEidFp() (err error)                  //手动获取eid和fp
+	Reservation() (err error)                        //预约
+	//SnapUp() ()											//抢购
+}
+
+type UserInfo struct {
+	HouseholdAppliance int    `json:"householdAppliance"`
+	ImgUrl             string `json:"imgUrl"`
+	LastLoginTime      string `json:"lastLoginTime"`
+	NickName           string `json:"nickName"`
+	PlusStatus         string `json:"plusStatus"`
+	RealName           string `json:"realName"`
+	UserLevel          int    `json:"userLevel"`
+	UserScoreVO        struct {
+		AccountScore     int    `json:"accountScore"`
+		ActivityScore    int    `json:"activityScore"`
+		ConsumptionScore int    `json:"consumptionScore"`
+		Default          bool   `json:"default"`
+		FinanceScore     int    `json:"financeScore"`
+		Pin              string `json:"pin"`
+		RiskScore        int    `json:"riskScore"`
+		TotalScore       int    `json:"total_score"`
+	} `json:"userScoreVO"`
 }
 
 type JdInfo struct {
-	UserNmae    string
-	LoginStatus bool
-	CookiesJar  *cookiejar.Jar
+	UserInfo          UserInfo
+	LoginStatus       bool
+	ReservationStatus bool
+	GoodsInfo         string
+	CookiesJar        *cookiejar.Jar
+	Eid               string
+	Fp                string
 }
+
+var Headers map[string]string = map[string]string{"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+	"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	"Connection":                "keep-alive",
+	"Cache-Control":             "max-age=0",
+	"Upgrade-Insecure-Requests": "1",
+	"Accept-Language":           "zh-CN,zh;q=0.9"}
+
+var GetEidFpHtml string = `<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+<div >eid:</div>
+<div id="eid"></div>
+<div >fp:</div>
+<div id="fp"></div>
+<div id="end"></div>
+</body>
+</html>
+
+<script src="https://gias.jd.com/js/td.js"></script>
+
+<script>
+    setTimeout(function () {
+        try {
+            getJdEid(function (eid, fp, udfp) {
+                document.getElementById('eid').innerText = eid;
+                document.getElementById('fp').innerText = fp;
+                document.getElementById('end').innerHTML = '<form id="endok"></form>';
+            });
+        } catch (e) {
+            document.getElementById('info').innerText = e;
+        }
+    }, 1000);
+</script>`
+
+var CookiesFile string = "jd.cookies"
 
 func (Tools *JdInfo) GetLoginQrcode() (qrcode image.Image, err error) {
 	var request *http.Request
 	var response *http.Response
 
 	//Tools.CookiesJar,_ = cookiejar.New(nil)
-	Tools.CookiesJar = url_tools.InitCookieJar(Tools.CookiesJar)
+	Tools.CookiesJar, err = url_tools.InitCookieJar(Tools.CookiesJar, CookiesFile)
+	if err != nil {
+		return
+	}
 	//fmt.Println(Tools.CookiesJar)
 
 	client := &http.Client{CheckRedirect: nil, Jar: Tools.CookiesJar}
 	// 打开登录页
 	LoginUrl := "https://passport.jd.com/new/login.aspx"
 	request, _ = http.NewRequest("GET", LoginUrl, nil)
-	request = url_tools.AddHeader(request)
+	request = url_tools.AddHeader(request, Headers)
 	response, err = client.Do(request)
-	defer response.Body.Close()
 	if err != nil {
 		return
 	}
+	defer response.Body.Close()
 	//fmt.Println(response.Cookies())
 	//Tools.Client.Jar.SetCookies(response.Request.URL,response.Cookies())
 
@@ -56,16 +132,16 @@ func (Tools *JdInfo) GetLoginQrcode() (qrcode image.Image, err error) {
 	parse.RawQuery = QueryValues.Encode()
 	//fmt.Println(parse.String())
 	request, _ = http.NewRequest("GET", parse.String(), nil)
-	request = url_tools.AddHeader(request)
+	request = url_tools.AddHeader(request, Headers)
 	request.Header.Add("Referer", "https://passport.jd.com/")
 	//url_tools.AddCookies(request,response.Cookies())
 	//fmt.Println(request)
 	response, err = client.Do(request)
 	//response2,err = http.Get("https://qr.m.jd.com/show?appid=133&size=147&t=1609429653561")
-	defer response.Body.Close()
 	if err != nil {
 		return
 	}
+	defer response.Body.Close()
 	//Tools.Client.Jar.SetCookies(response.Request.URL,response.Cookies())
 	if response.StatusCode == 200 {
 		var n int
@@ -113,23 +189,27 @@ func (Tools *JdInfo) GetLoginQrcode() (qrcode image.Image, err error) {
 	return
 }
 
-func (Tools *JdInfo) CheckLogin() (err error) {
+func (Tools *JdInfo) CheckLogin() (code int, err error) {
 	type QrCheckInfo struct {
-		Code int    `json:"code"`
-		Msg  string `json:"msg"`
+		Code   int    `json:"code"`
+		Msg    string `json:"msg"`
+		Ticket string `json:"ticket"`
+	}
+	type qrCodeTicketValidation struct {
+		ReturnCode int `json:"returnCode"`
 	}
 	var request *http.Request
 	var response *http.Response
 	var token string
 	var readAll []byte
 	var qcinfo QrCheckInfo
+	var qctv qrCodeTicketValidation
 
 	client := &http.Client{CheckRedirect: nil, Jar: Tools.CookiesJar}
-	QrcodeUrl := "https://qr.m.jd.com/check"
+	Url := "https://qr.m.jd.com/check"
 	//var n int = 85
 	for {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		u, _ := url.Parse(QrcodeUrl)
+		u, _ := url.Parse(Url)
 		cookies := Tools.CookiesJar.Cookies(u)
 		for _, v := range cookies {
 			if v.Name == "wlfstk_smdl" {
@@ -137,20 +217,23 @@ func (Tools *JdInfo) CheckLogin() (err error) {
 				break
 			}
 		}
-		var QueryValues url.Values = map[string][]string{"appid": []string{"133"}, "callback": []string{"jQuery" + strconv.Itoa(r.Int())[:9]}, "token": []string{token}, "_": []string{strconv.FormatInt(time.Now().UnixNano(), 10)[:13]}}
-		parse, _ := url.Parse(QrcodeUrl)
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		var callback = "jQuery" + strconv.Itoa(r.Int())[:9]
+
+		var QueryValues url.Values = map[string][]string{"appid": []string{"133"}, "callback": []string{callback}, "token": []string{token}, "_": []string{strconv.FormatInt(time.Now().UnixNano(), 10)[:13]}}
+		parse, _ := url.Parse(Url)
 		parse.RawQuery = QueryValues.Encode()
 		//fmt.Println(parse.String())
 		request, _ = http.NewRequest("GET", parse.String(), nil)
-		request = url_tools.AddHeader(request)
+		request = url_tools.AddHeader(request, Headers)
 		request.Header.Add("Referer", "https://passport.jd.com/")
 		//url_tools.AddCookies(request,QrcodeCookies)
 		//fmt.Println(request)
 		response, err = client.Do(request)
-		defer response.Body.Close()
 		if err != nil {
 			return
 		}
+		defer response.Body.Close()
 		//Tools.Client.Jar.SetCookies(response.Request.URL,response.Cookies())
 
 		readAll, err = ioutil.ReadAll(response.Body)
@@ -158,6 +241,7 @@ func (Tools *JdInfo) CheckLogin() (err error) {
 			return
 		}
 		//fmt.Println(string(readAll))
+		//fmt.Println(Tools.CookiesJar)
 		compile := regexp.MustCompile(`(?s:\{.*\})`)
 		findString := compile.FindString(string(readAll))
 
@@ -165,13 +249,53 @@ func (Tools *JdInfo) CheckLogin() (err error) {
 		if err != nil {
 			return
 		}
+		code = qcinfo.Code
 		// 201:二维码未扫描，请扫描二维码	202:请手机客户端确认登录	203:二维码过期，请重新扫描	205:二维码已取消授权
 		if qcinfo.Code == 201 || qcinfo.Code == 202 {
 			//err = fmt.Errorf("err_msg: '%s'",qcinfo.Msg)
 			continue
 		} else if qcinfo.Code == 200 {
 			//fmt.Println(Tools.CookiesJar)
-			return
+			Url := "https://passport.jd.com/uc/qrCodeTicketValidation"
+			var QueryValues url.Values = map[string][]string{"t": []string{qcinfo.Ticket}}
+			parse, _ := url.Parse(Url)
+			parse.RawQuery = QueryValues.Encode()
+			//fmt.Println(parse.String())
+			request, _ = http.NewRequest("GET", parse.String(), nil)
+			request = url_tools.AddHeader(request, Headers)
+			request.Header.Add("Referer", "https://passport.jd.com/uc/login?ltype=logout")
+			//url_tools.AddCookies(request,response.Cookies())
+			//fmt.Println(request)
+			response, err = client.Do(request)
+			//response2,err = http.Get("https://qr.m.jd.com/show?appid=133&size=147&t=1609429653561")
+			if err != nil {
+				return
+			}
+			defer response.Body.Close()
+			readAll, err = ioutil.ReadAll(response.Body)
+			if err != nil {
+				return
+			}
+			err = json.Unmarshal([]byte(readAll), &qctv)
+			if err != nil {
+				return
+			}
+			if qctv.ReturnCode == 0 {
+				// 获取用户信息后保存cookice
+				//err = Tools.GetUserInfo()
+				//if err != nil {
+				//	return
+				//}
+				Tools.LoginStatus = true
+				err = Tools.CookiesJar.Save()
+				//fmt.Println(Tools.CookiesJar)
+				//fmt.Println("---------------------------")
+				//err = url_tools.SaveCookie("Jd", Tools.UserInfo.NickName, Tools.CookiesJar)
+				return
+			} else {
+				err = fmt.Errorf("二维码信息校验失败.")
+				return
+			}
 		} else if qcinfo.Code == 203 || qcinfo.Code == 205 {
 			err = fmt.Errorf("code: %d ,err_msg: '%s'", qcinfo.Code, qcinfo.Msg)
 			return
@@ -181,11 +305,152 @@ func (Tools *JdInfo) CheckLogin() (err error) {
 		}
 		time.Sleep(2 * time.Second)
 	}
-
 	return
 }
 
-func (Tools *JdInfo) CheckCookies() (status bool) {
+func (Tools *JdInfo) GetUserInfo() (err error) {
+	var request *http.Request
+	var response *http.Response
+	var readAll []byte
+
+	client := &http.Client{CheckRedirect: nil, Jar: Tools.CookiesJar}
+
+	Url := "https://passport.jd.com/user/petName/getUserInfoForMiniJd.action"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var QueryValues url.Values = map[string][]string{"callback": []string{"jQuery" + strconv.Itoa(r.Int())[:7]}, "_": []string{strconv.FormatInt(time.Now().UnixNano(), 10)[:13]}}
+	parse, _ := url.Parse(Url)
+	parse.RawQuery = QueryValues.Encode()
+	request, _ = http.NewRequest("GET", parse.String(), nil)
+	request = url_tools.AddHeader(request, Headers)
+	request.Header.Add("Referer", "https://order.jd.com/center/list.action")
+	response, err = client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	readAll, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+
+	compile := regexp.MustCompile(`(?s:\{.*\})`)
+	allString := compile.FindString(string(readAll))
+	err = json.Unmarshal([]byte(allString), &Tools.UserInfo)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (Tools *JdInfo) CheckCookies() (code int, err error) {
+	var request *http.Request
+	var response *http.Response
+	Tools.CookiesJar, err = url_tools.InitCookieJar(Tools.CookiesJar, CookiesFile)
+	if err != nil {
+		return
+	}
+	client := &http.Client{CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}, Jar: Tools.CookiesJar}
+
+	Url := "https://order.jd.com/center/list.action"
+	var QueryValues url.Values = map[string][]string{"rid": []string{strconv.FormatInt(time.Now().UnixNano(), 10)[:13]}}
+	parse, _ := url.Parse(Url)
+	parse.RawQuery = QueryValues.Encode()
+	request, _ = http.NewRequest("GET", parse.String(), nil)
+	request = url_tools.AddHeader(request, Headers)
+	response, err = client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	code = response.StatusCode
+	//fmt.Println(Tools.CookiesJar)
+	//fmt.Println(response.Request.URL,response.Status)
+	if code == 200 {
+		Tools.LoginStatus = true
+	} else if code == 302 {
+		err = fmt.Errorf("cookices 已过期，请重新登录.")
+	} else {
+		err = fmt.Errorf("检查cookices时发生异常.")
+	}
+	return
+}
+
+func (Tools *JdInfo) AutoObtainEidFp() (err error) {
+	var allocCtx = context.Background()
+	// 关闭无头模式
+	//opts := append(chromedp.DefaultExecAllocatorOptions[:],
+	//	chromedp.Flag("headless", false),
+	//)
+	//allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	//defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	ts := httptest.NewServer(url_tools.WriteHTML(GetEidFpHtml))
+	defer ts.Close()
+	if err = chromedp.Run(ctx,
+		chromedp.Navigate(ts.URL),
+		chromedp.WaitVisible(`#endok`, chromedp.ByID),
+		chromedp.Text(`#eid`, &Tools.Eid),
+		chromedp.Text(`#fp`, &Tools.Fp),
+	); err != nil {
+		return
+	}
+	if Tools.Eid == "" || Tools.Fp == "" {
+		err = Tools.AutoObtainEidFp()
+	}
+	return
+}
+
+func (Tools *JdInfo) ManualObtainEidFp() (err error) {
+	var filepath string
+	filepath, err = url_tools.SaveHtml("geteidfp", GetEidFpHtml)
+	if err != nil {
+		return
+	}
+	err = webbrowser.Open("file://" + filepath)
+	return
+}
+
+func (Tools *JdInfo) Reservation(JdUrl string) (err error) {
+	var goodsId int
+	var request *http.Request
+	var response *http.Response
+	var readAll []byte
+
+	compile := regexp.MustCompile(`^(http://|https://)?item.jd.com/([0-9]{6,})\.html$`)
+	urlfromat := compile.MatchString(JdUrl)
+	if !urlfromat {
+		err = fmt.Errorf("京东url格式有误.")
+		return
+	} else {
+		goodsId, _ = strconv.Atoi(compile.FindAllStringSubmatch(JdUrl, 1)[0][2])
+	}
+
+	client := &http.Client{CheckRedirect: nil, Jar: Tools.CookiesJar}
+
+	Url := "https://yushou.jd.com/youshouinfo.action"
+	var QueryValues url.Values = map[string][]string{"suk": []string{strconv.Itoa(goodsId)}}
+	parse, _ := url.Parse(Url)
+	parse.RawQuery = QueryValues.Encode()
+	request, _ = http.NewRequest("GET", parse.String(), nil)
+	request = url_tools.AddHeader(request, Headers)
+	request.Header.Add("Referer", fmt.Sprintf("https://item.jd.com/%d.html", goodsId))
+	response, err = client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	//fmt.Println(goodsId)
+	readAll, err = ioutil.ReadAll(response.Body)
+
+	err = json.Unmarshal([]byte(readAll), &Tools.UserInfo)
+	if err != nil {
+		return
+	}
 
 	return
 }
--- pkg/url_tools/urltools.go
+++ pkg/url_tools/urltools.go
@@ -1,31 +1,196 @@
 package url_tools
 
 import (
+	"fmt"
+	cookiejar "github.com/orirawlings/persistent-cookiejar"
+	"io"
+	"io/ioutil"
 	"net/http"
-	"net/http/cookiejar"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	//cookiejar "github.com/juju/persistent-cookiejar"
+	"os"
 )
 
-func AddHeader(request *http.Request) (NewRequest *http.Request) {
-	request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-	request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-	request.Header.Add("Connection", "keep-alive")
-	request.Header.Add("Cache-Control", "max-age=0")
-	request.Header.Add("Upgrade-Insecure-Requests", "1")
-	request.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
+var CacheDir = "cache"
+
+func AddHeader(request *http.Request, headers map[string]string) (NewRequest *http.Request) {
+	//request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+	//request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	//request.Header.Add("Connection", "keep-alive")
+	//request.Header.Add("Cache-Control", "max-age=0")
+	//request.Header.Add("Upgrade-Insecure-Requests", "1")
+	//request.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
+	for k, v := range headers {
+		request.Header.Add(k, v)
+	}
 	//request.Header.Add("Accept-Encoding","gzip, deflate, br")
 	return request
 }
 
-func InitCookieJar(Cookiejar *cookiejar.Jar) (NewCookiejar *cookiejar.Jar) {
+func InitCookieJar(Cookiejar *cookiejar.Jar, File string) (NewCookiejar *cookiejar.Jar, err error) {
 	if Cookiejar == nil {
-		NewCookiejar, _ = cookiejar.New(nil)
+		//NewCookiejar, _ = cookiejar.New(nil)
+		//var stat os.FileInfo
+		//if stat, err = os.Stat(CacheDir); err != nil && os.IsExist(err) {
+		//	return
+		//} else if os.IsNotExist(err) {
+		//	err = os.MkdirAll(CacheDir, os.ModePerm)
+		//	if err != nil {
+		//		return
+		//	}
+		//} else if ! stat.IsDir() {
+		//	err = fmt.Errorf("目录'%s'已存在，且是个文件，请检查。",CacheDir)
+		//	return
+		//}
+		err = CreateDir(CacheDir)
+		if err != nil {
+			return
+		}
+		var path string
+		path, err = AsbPath()
+		if err != nil {
+			return
+		}
+		// 调试
+		path = "."
+
+		NewCookiejar, _ = cookiejar.New(&cookiejar.Options{Filename: path + "/" + CacheDir + "/" + File, PersistSessionCookies: true})
+		//NewCookiejar, _ = cookiejar.New(nil)
+		return
+	}
+	NewCookiejar = Cookiejar
+	return
+}
+
+func WriteHTML(content string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		io.WriteString(w, strings.TrimSpace(content))
+	})
+}
+
+func SaveHtml(filename string, content string) (filepath string, err error) {
+	//var file *os.File
+	err = CreateDir(CacheDir)
+	if err != nil {
+		return
+	}
+	var path string
+	path, err = AsbPath()
+	if err != nil {
+		return
+	}
+	// 调试
+	path = "."
+	//file, err = os.OpenFile(path+"/"+CacheDir+"/"+filename+".html", os.O_CREATE|os.O_RDWR, 0640)
+	//if err != nil {
+	//	return
+	//}
+	//defer file.Close()
+	filepath = path + "/" + CacheDir + "/" + filename + ".html"
+	err = ioutil.WriteFile(filepath, []byte(content), 0640)
+	//if err != nil {
+	//	return
+	//}
+	return
+}
+
+func CreateDir(dir string) (err error) {
+	var stat os.FileInfo
+	if stat, err = os.Stat(CacheDir); err != nil && os.IsExist(err) {
+		return
+	} else if os.IsNotExist(err) {
+		err = os.MkdirAll(CacheDir, os.ModePerm)
+		if err != nil {
+			return
+		}
+	} else if !stat.IsDir() {
+		err = fmt.Errorf("目录'%s'已存在,且是个文件,请检查.", CacheDir)
+		return
 	}
 	return
 }
 
-func AddCookies(request *http.Request, cookies []*http.Cookie) (NewRequest *http.Request) {
-	for _, v := range cookies {
-		request.AddCookie(v)
-	}
+func AsbPath() (path string, err error) {
+	// 获取可执行文件相对于当前工作目录的相对路径
+	path = filepath.Dir(os.Args[0])
+	// 根据相对路径获取可执行文件的绝对路径
+	path, err = filepath.Abs(path)
 	return
 }
+
+func FindChromePath() string {
+	for _, path := range [...]string{
+		// Unix-like
+		"headless_shell",
+		"headless-shell",
+		"chromium",
+		"chromium-browser",
+		"google-chrome",
+		"google-chrome-stable",
+		"google-chrome-beta",
+		"google-chrome-unstable",
+		"/usr/bin/google-chrome",
+
+		// Windows
+		"chrome",
+		"chrome.exe", // in case PATHEXT is misconfigured
+		`C:\Program Files (x86)\Google\Chrome\Application\chrome.exe`,
+		`C:\Program Files\Google\Chrome\Application\chrome.exe`,
+		filepath.Join(os.Getenv("USERPROFILE"), `AppData\Local\Google\Chrome\Application\chrome.exe`),
+
+		// Mac
+		"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+	} {
+		found, err := exec.LookPath(path)
+		if err == nil {
+			return found
+		}
+	}
+	// Fall back to something simple and sensible, to give a useful error
+	// message.
+	return ""
+}
+
+//func SaveCookie(Prefix string,UserName string,Cookiejar *cookiejar.Jar) (err error) {
+//	//fmt.Println(response.Cookies())
+//	//f, _ := os.OpenFile("test.cookies", os.O_RDWR|os.O_CREATE, 0600)
+//	//defer f.Close()
+//	//enc := gob.NewEncoder(f)
+//	//enc.Encode(response.Cookies())
+//	//os.OpenFile("")
+//	//Cookiejar.Save()
+//	var stat os.FileInfo
+//	var f *os.File
+//	if stat, err = os.Stat(CacheDir); err != nil && os.IsExist(err) {
+//		err = fmt.Errorf("保存cookie时出错,ERROR: %s",err)
+//		return
+//	} else if os.IsNotExist(err) {
+//		err = os.MkdirAll(CacheDir, os.ModePerm)
+//		if err != nil {
+//			return
+//		}
+//	} else if ! stat.IsDir() {
+//		err = fmt.Errorf("'%s'文件已存在，且不是个目录。",CacheDir)
+//		return
+//	}
+//	f, err = os.OpenFile(fmt.Sprintf("%s-%s.cookie",Prefix,UserName), os.O_RDWR|os.O_CREATE, 0600)
+//	defer f.Close()
+//	if err != nil {
+//		return
+//	}
+//	enc := gob.NewEncoder(f)
+//	fmt.Println(Cookiejar)
+//	err = enc.Encode(Cookiejar)
+//	return
+//}
+
+//func AddCookies(request *http.Request, cookies []*http.Cookie) (NewRequest *http.Request) {
+//	for _, v := range cookies {
+//		request.AddCookie(v)
+//	}
+//	return
+//}
